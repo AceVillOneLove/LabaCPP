925dd3c27bcabd66da51af3397460288601cfb8f
DieOnRally <84383968+AceVillOneLove@users.noreply.github.com>
2021-11-28 16:44:14 +0300

    Add files via upload

---
+++ Laba 4.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int i = 0, k = 0;
+    string s;
+    printf("String:\n");
+    getline(cin, s);
+    while (s[i])
+    {
+        if (s[i] >= '0' && s[i] <= '9') {
+            k++;
+        }
+        i++;
+    }
+    if (k == i)
+        printf("The Number\n");
+    else
+        printf("Identifier\n");
+    system("pause");
+    return 0;
+}
